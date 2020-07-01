3bfdf8b7ed6113f3f0e4dcffc474708a5bba4c91
AmirahKPD <67449932+AmirahKPD@users.noreply.github.com>
2020-07-01 15:28:54 +0800

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?PHP
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "stts";
+
+$conn = mysqli_connect ($) or die("Unable to connect");
+
+echo "Great Work!!";
+
+?>
+
+
+		
+			
